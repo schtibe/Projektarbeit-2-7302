909be3f92ed56a7bd817dd035edd56fc875b76b0
choschi <choschi@blackshark.(none)>
2011-01-11 16:46:20 +0100

    changed the crossesMe methods in the Direction objects to be working now

--- src/driver/StraightDirection.java
+++ src/driver/StraightDirection.java
@@ -1,28 +1,31 @@
 package driver;
 
 import environment.IPriority;
+/**
+ * to whom it may concern: sorry for the instanceofs 8(
+ */
 
 public class StraightDirection implements IDirection {
 
 	@Override
 	public boolean crossesMe(IDirection comingFrom, IDirection goingTo) {
+		if (comingFrom instanceof LeftDirection){
+			return comingFromLeft (goingTo);
+		}else if (comingFrom instanceof RightDirection){
+			return true;
+		}else{
+			if (goingTo instanceof LeftDirection){
+				return true;
+			}
+		}
 		return false;
 	}
 	
-	public boolean crossesMe(StraightDirection comingFrom, LeftDirection goingTo){
-		return true;
-	}
-	
-	public boolean crossesMe (RightDirection comingFrom, IDirection goingTo){
-		return true;
-	}
-	
-	public boolean crossesMe (LeftDirection comingFrom, LeftDirection goingTo){
-		return true;
-	}
-	
-	public boolean crossesMe (LeftDirection comingFrom, StraightDirection goingTo){
-		return true;
+	private boolean comingFromLeft(IDirection goingTo) {
+		if (!(goingTo instanceof RightDirection)){
+			return true;
+		}
+		return false;
 	}
 	
 	public String toString (){
